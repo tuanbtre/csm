9131f2e84e4750d75051de0d1fbebeb73a2a1356
Tuan Nguyen Minh <tuannm@tdt-tanduc.com>
2022-05-25 17:18:01 +0700

    update logo login admin

--- src/Resources/Admin/login.blade.php
+++ src/Resources/Admin/login.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <title>Login </title>
+    <link rel="shortcut icon" href="{{asset('logo.ico')}}"/>
     <link rel="stylesheet" href="{{asset('vendor/csm/css/login.css')}}">
     <link rel="stylesheet" href="{{asset('vendor/csm/css/bootstrap.min.css')}}">
     <link rel="stylesheet" type="text/css" href="{{asset('vendor/csm/css/font-awesome.css')}}">  
@@ -16,6 +17,9 @@
 				@csrf
 				<div class="login100-form-logo p-b-30">
 					<img src="{{asset('images/logo_admin.png')}}" alt=""/> 
+				</div>
+				<div class="login100-form-logo p-b-30">
+					<h5 style="font-size:18px; color: #fff; text-align:center;">Đăng nhập hệ thống quản trị website {!!env('APP_NAME')!!} </h5>
 				</div>
 				<div class="wrap-input100 validate-input m-b-10" data-validate = "Username is required">
 					<input class="input100 {{$errors->has('username') ? ' is-invalid' : ''}}" type="text" name="username" required value="{{ old('username') }}" placeholder="Tên đăng nhập">
